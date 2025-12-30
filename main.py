ef724322125667b5399d2f6789ab7bd06bab87a4
unknown <fog009922[@gmail.com](mailto:you@example.com)>
2025-12-30 16:22:26 +0900

    버튼 누를 시 안내 창 전송

--- main.py
+++ main.py
@@ -3,15 +3,21 @@ from tkinter import messagebox
 
 #버튼 클래스 만들기
 class button:
-    def __init__(self, main, _text, row, column):
+    def __init__(self, main, _text, row, column, msg="info"):
         self.main = main
         self.text = _text
         self.row = row
         self.column = column
+        self.msg = msg
 
+    def click_warn(self):
+        if self.msg == "종료":
+         messagebox.showwarning("경고", "누르지 말라 했잖아")
+        else:
+            messagebox.showinfo("알림", "다른 거 눌러")
+    
     def maker (self):
-        tk.Button(self.main, text=self.text).grid(row=self.row, column=self.column)
-
+        tk.Button(self.main, text=self.text, command=self.click_warn).grid(row=self.row, column=self.column)
 #글자 클래스 라벨 만들기
 class label_maker:
     def __init__(self, main, _text, row, column):
@@ -19,16 +25,10 @@ class label_maker:
         self.text = _text
         self.row = row
         self.column = column
-        
+
     def maker (self):
         tk.Label(self.main, text=self.text).grid(row=self.row, column=self.column)
 
-#경고문 만들기
-def warning():
-    messagebox.showwarning("경고", "눌르지 말라고")
-
-def clear():
-    messagebox.showinfo("완료", "OKAY")
 
 root = tk.Tk()
 root.title("class 연습")
@@ -40,13 +40,15 @@ labe1.maker()
 button1 = button(root, "버튼 1", 0, 1)
 button1.maker()
 
+
 button3 = button(root, "버튼 3", 0, 2)
 button3.maker()
+
 
 labe2 = label_maker(root, "눌르지마", 1, 0)
 labe2.maker()
 
-button2 = button(root, "버튼 2", 1, 1)
+button2 = button(root, "버튼 2", 1, 1, "종료")
 button2.maker()
 
 
