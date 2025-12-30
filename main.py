75b34f07e577f112cdfbf55248a9973db9dd9302
unknown <fog009922[@gmail.com](mailto:you@example.com)>
2025-12-30 15:55:43 +0900

    라벨 공장 생성 완료

--- main.py
+++ main.py
@@ -14,26 +14,33 @@ class button:
 
 #글자 클래스 라벨 만들기
 class label_maker:
-    def __inti__(self, main, _text, row, column):
+    def __init__(self, main, _text, row, column):
         self.main = main
         self.text = _text
         self.row = row
         self.column = column
         
     def maker (self):
-        tk.Label(self.main, text=self.text, row=self.row, column=self.column)
-        
+        tk.Label(self.main, text=self.text).grid(row=self.row, column=self.column)
+
 root = tk.Tk()
 root.title("class 연습")
 root.geometry("600x400")
 
-button1 = button(root, "버튼 1", 0, 0)
+labe1 = label_maker(root, "눌러", 0, 0)
+labe1.maker()
+
+button1 = button(root, "버튼 1", 0, 1)
 button1.maker()
 
-button2 = button(root, "버튼 2", 1, 0)
+button3 = button(root, "버튼 3", 0, 2)
+button3.maker()
+
+labe2 = label_maker(root, "눌르지마", 1, 0)
+labe2.maker()
+
+button2 = button(root, "버튼 2", 1, 1)
 button2.maker()
 
-button3 = button(root, "버튼 3", 0, 1)
-button3.maker()
 
 root.mainloop()
