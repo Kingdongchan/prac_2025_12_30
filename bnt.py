f92ff9cc90143425e5e0da0d8764375d9a2f8c28
unknown <fog009922[@gmail.com](mailto:you@example.com)>
2025-12-30 17:30:54 +0900

    숫자 넣기 실패

--- bnt.py
+++ bnt.py
@@ -2,6 +2,13 @@ import tkinter as tk
 
 root = tk.Tk()
 
+#버튼을 누르면 창에 들어가도록하는 함수 설정
+def push():
+    botton1 = btn1.get()
+    botton2 = btn2.get()
+    
+    entry.insert(0, botton1)
+    entry.insert(0, botton2)
 
 #버튼 2개와 입력창 생성
 entry = tk.Entry(root, state="readonly")
