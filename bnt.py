4b6ece894b0860d2d1e41fe111840a006d44814e
unknown <fog009922[@gmail.com](mailto:you@example.com)>
2025-12-30 17:38:38 +0900

    버튼을 눌렀을 떄 입력 창에 숫자 입력 완료

--- bnt.py
+++ bnt.py
@@ -10,15 +10,15 @@ def push(num):
     entry.insert(tk.END, num)
     #입력창을 작성하지 못하게 원상태로 복귀
     entry.config(state="readonly")
-    
+
 #버튼 2개와 입력창 생성
 entry = tk.Entry(root, state="readonly")
 entry.grid(row=0, column=0, columnspan=2)
-
-btn1 = tk.Button(root, text="1", padx= 10, pady=10)
+# 람다 -> 이름없는 일회용 함수
+btn1 = tk.Button(root, text="1", padx= 10, pady=10, command=lambda: push(1))
 btn1.grid(row=1, column=0)
 
-btn2 = tk.Button(root, text="2", padx=10, pady=10)
+btn2 = tk.Button(root, text="2", padx=10, pady=10, command=lambda: push(2))
 btn2.grid(row=1, column=1)
 
 
