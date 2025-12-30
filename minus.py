1aee1c5cf3918e2376dcc38a920f88e548a2c8e2
unknown <fog009922[@gmail.com](mailto:you@example.com)>
2025-12-30 16:49:05 +0900

    수 입력와 결과를 위한 3개의 입력칸와 버튼 생성

--- minus.py
+++ minus.py
@@ -4,4 +4,14 @@ root = tk.Tk()
 root.title("뺄셈")
 root.geometry("400x200")
 
+#뺄셈을 위해 입력 칸 3개 만들기
+tk.Entry(root).grid(row=0, column=0, padx=10)
+
+tk.Entry(root).grid(row=0, column=1, padx=10)
+
+#계산 버튼만들기
+tk.Button(root, text="=", command=equl).grid(row=0, column=2, padx=10)
+
+tk.Enry(root).grid(row=0, column=3, padx=10)
+
 root.mainloop()
