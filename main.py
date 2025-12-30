7d21760ce0f1d4b6ff7b1d6f99191609504c8e1f
unknown <fog009922[@gmail.com](mailto:you@example.com)>
2025-12-30 15:52:03 +0900

    라벨 공장 만들기

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import tkinter as tk
+from tkinter import messagebox
 
 #버튼 클래스 만들기
 class button:
-    def __init__(self, name, main, _text, row, column):
-        self.name = name
+    def __init__(self, main, _text, row, column):
         self.main = main
         self.text = _text
         self.row = row
@@ -12,18 +12,28 @@ class button:
     def maker (self):
         tk.Button(self.main, text=self.text).grid(row=self.row, column=self.column)
 
-
+#글자 클래스 라벨 만들기
+class label_maker:
+    def __inti__(self, main, _text, row, column):
+        self.main = main
+        self.text = _text
+        self.row = row
+        self.column = column
+        
+    def maker (self):
+        tk.Label(self.main, text=self.text, row=self.row, column=self.column)
+        
 root = tk.Tk()
 root.title("class 연습")
 root.geometry("600x400")
 
-button1 = button("bt1", root, "버튼 1", 0, 0)
+button1 = button(root, "버튼 1", 0, 0)
 button1.maker()
 
-button2 = button("bt2", root, "버튼 2", 1, 0)
+button2 = button(root, "버튼 2", 1, 0)
 button2.maker()
 
-button3 = button("bt3", root, "버튼 3", 0, 1)
+button3 = button(root, "버튼 3", 0, 1)
 button3.maker()
 
 root.mainloop()
