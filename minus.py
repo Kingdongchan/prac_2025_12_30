5435047f1f974bf03d4ea84c191e0021564faf1b
unknown <fog009922[@gmail.com](mailto:you@example.com)>
2025-12-30 16:53:29 +0900

    뺼셈 완성

--- minus.py
+++ minus.py
@@ -4,14 +4,28 @@ root = tk.Tk()
 root.title("뺄셈")
 root.geometry("400x200")
 
-#뺄셈을 위해 입력 칸 3개 만들기
-tk.Entry(root).grid(row=0, column=0, padx=10)
+#계산하기
+#입력한 값을 먼저 가져오기
+def minus():
+    first = int(entry1.get())
+    second = int(entry2.get())
 
-tk.Entry(root).grid(row=0, column=1, padx=10)
+    value = first - second
+
+    result.delete(0, tk.END)
+    result.insert(0, value)
+
+#뺄셈을 위해 입력 칸 3개 만들기
+entry1 =tk.Entry(root)
+entry1.grid(row=0, column=0, padx=10)
+
+entry2 = tk.Entry(root)
+entry2.grid(row=0, column=1, padx=10)
 
 #계산 버튼만들기
-tk.Button(root, text="=", command=equl).grid(row=0, column=2, padx=10)
+tk.Button(root, text="=", command=minus).grid(row=0, column=2, padx=10)
 
-tk.Enry(root).grid(row=0, column=3, padx=10)
+result = tk.Entry(root)
+result.grid(row=0, column=3, padx=10)
 
 root.mainloop()
